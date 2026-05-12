01c139f67656f28bea1a6117ed8a1a329c026072
Kirya Duncan II <kirya.duncan@protonmail.com>
2026-05-12 10:40:59 -0400

    Added javadocs to all public Model members
    - overrided methods already had javadocs in their base classes

--- src/main/java/kirya/model/Question.java
+++ src/main/java/kirya/model/Question.java
@@ -6,6 +6,9 @@ import java.util.SequencedCollection;
 import kirya.utils.DisplayableQuestion;
 import kirya.utils.QuestionType;
 
+/**
+ * A dataclass that stores the necessary information for a question.
+ */
 public class Question extends DisplayableQuestion {
 
     private QuestionType questionType;
@@ -13,6 +16,14 @@ public class Question extends DisplayableQuestion {
     private SequencedCollection<String> choices;
     private SequencedCollection<String> answers;
 
+    /**
+     * Initializes a new Question object with default state.
+     * 
+     * <p>Postcondition: this.getQuestionType() == QuestionType.FREE_RESPONSE
+     * <p>Postcondition: this.getQuestion() == ""
+     * <p>Postcondition: this.getChoices() == {}
+     * <p>Postcondition: this.getAnswers() == {}
+     */
     public Question() {
         this.questionType = QuestionType.FREE_RESPONSE;
         this.question = "";
@@ -20,18 +31,34 @@ public class Question extends DisplayableQuestion {
         this.answers = new ArrayList<>();
     }
 
+    /**
+     * Sets the type of question this Question is.
+     * @param questionType The new type of question
+     */
     public void setQuestionType(QuestionType questionType) {
         this.questionType = questionType;
     }
 
+    /**
+     * Sets the question
+     * @param question The new question being asked
+     */
     public void setQuestion(String question) {
         this.question = question;
     }
 
+    /**
+     * Sets the answer choices.
+     * @param answerChoices The new answer choices
+     */
     public void setChoices(SequencedCollection<String> answerChoices) {
         this.choices = answerChoices;
     }
 
+    /**
+     * Sets the correct answers.
+     * @param correctAnswers The new correct answers
+     */
     public void setAnswers(SequencedCollection<String> correctAnswers) {
         this.answers = correctAnswers;
     }
--- src/main/java/kirya/model/StudyGuide.java
+++ src/main/java/kirya/model/StudyGuide.java
@@ -8,6 +8,9 @@ import java.util.Set;
 import kirya.utils.DisplayableQuestion;
 import kirya.utils.DisplayableStudyGuide;
 
+/**
+ * A dataclass that stores the necessary information for a study guide.
+ */
 public class StudyGuide extends DisplayableStudyGuide {
 
     private boolean isFavorited;
@@ -15,6 +18,14 @@ public class StudyGuide extends DisplayableStudyGuide {
     private String description;
     private final Set<Question> questions;
 
+    /**
+     * Initializes a StudyGuide with default state.
+     * 
+     * <p>Postcondition: this.getIsFavorited() == false
+     * <p>Postcondition: this.getTitle() == ""
+     * <p>Postcondition: this.getDescription() == ""
+     * <p>Postcondition: this.getQuestions() == {}
+     */
     public StudyGuide() {
         this.isFavorited = false;
         this.title = "";
@@ -22,20 +33,36 @@ public class StudyGuide extends DisplayableStudyGuide {
         this.questions = new LinkedHashSet<>();
     }
 
+    /**
+     * Sets the title.
+     * @param title The new title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
     
+    /**
+     * Sets the description.
+     * @param description The new description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Sets the questions.
+     * @param questions The new questions
+     */
     public void setQuestions(Collection<Question> questions) {
         this.questions.clear();
         this.questions.addAll(questions);
     }
 
-    public void setFavorited(boolean b) {
+    /**
+     * Sets if this is favorited.
+     * @param b The new favorited state
+     */
+    public void setIsFavorited(boolean b) {
         this.isFavorited = b;
     }
 
