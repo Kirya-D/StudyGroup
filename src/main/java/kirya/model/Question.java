0affe5ef4d71f9ca2b3a94010897eb33e2a660f7
Kirya Duncan II <kirya.duncan@protonmail.com>
2026-05-16 16:32:06 -0400

    Testing for StudyGuide & Question Added unit tests for the classes StudyGuide and Question

--- src/main/java/kirya/model/Question.java
+++ src/main/java/kirya/model/Question.java
@@ -34,32 +34,48 @@ public class Question extends DisplayableQuestion {
     /**
      * Sets the type of question this Question is.
      * @param questionType The new type of question
+     * @throws IllegalArgumentException If questionType is null
      */
     public void setQuestionType(QuestionType questionType) {
+        if (questionType == null) {
+            throw new IllegalArgumentException("questionType can't be null");
+        }
         this.questionType = questionType;
     }
 
     /**
      * Sets the question
      * @param question The new question being asked
+     * @throws IllegalArgumentException If answerChoices is null or blank as determined by {@code String.isBlank()}
      */
     public void setQuestion(String question) {
+        if (question == null || question.isBlank()) {
+            throw new IllegalArgumentException("question must be a non-blank string");
+        }
         this.question = question;
     }
 
     /**
      * Sets the answer choices.
      * @param answerChoices The new answer choices
+     * @throws IllegalArgumentException If answerChoices is null
      */
     public void setChoices(SequencedCollection<String> answerChoices) {
+        if (answerChoices == null) {
+            throw new IllegalArgumentException("answerChoices can't be null");
+        }
         this.choices = answerChoices;
     }
 
     /**
      * Sets the correct answers.
      * @param correctAnswers The new correct answers
+     * @throws IllegalArgumentException If correctAnswers is null
      */
     public void setAnswers(SequencedCollection<String> correctAnswers) {
+        if (correctAnswers == null) {
+            throw new IllegalArgumentException("correctAnswers can't be null");
+        }
         this.answers = correctAnswers;
     }
 
